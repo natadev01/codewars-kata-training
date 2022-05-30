25faf3bb50060f84d3b01fd7849bc6dc40e98182
Natalia Polanco <96508629+natadev01@users.noreply.github.com>
2022-05-30 13:44:16 -0400

    Create duplicate.py

---
+++ duplicate.py
@@ -0,0 +1,11 @@
+
+def duplicate_count(text):
+    n = 0
+    textSorted = sorted(text)
+    
+    for i in range(len(textSorted)):
+        if textSorted[ i] != textSorted[ i - 1 ] and i+1 < len(textSorted):
+            if textSorted[ i ] == textSorted[ i + 1 ]:
+                n+= 1
+    return n
+print(duplicate_count("Indivisibilities"))
