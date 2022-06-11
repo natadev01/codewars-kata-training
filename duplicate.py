7bbe84ac9072e02a5a07dd0bc54ec59bd4c986f0
Natalia Polanco <96508629+natadev01@users.noreply.github.com>
2022-06-11 19:01:57 -0400

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,7 +1,7 @@
 
 def duplicate_count(text):
     n = 0
-    textSorted = sorted(text)
+    textSorted = sorted(text.upper())
     
     for i in range(len(textSorted)):
         if textSorted[ i] != textSorted[ i - 1 ] and i+1 < len(textSorted):
